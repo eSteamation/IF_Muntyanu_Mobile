0502f9f0c37d5d43fd97c0bc0881cd87dcd49a92
eSteamation <sdaark@gmail.com>
2025-06-10 00:26:08 +0300

    Исправление мелких ошибок.

--- src/main/java/ru/iFellow/Pages/BasePage.java
+++ src/main/java/ru/iFellow/Pages/BasePage.java
@@ -1,10 +1,13 @@
 package ru.iFellow.Pages;
 
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
 import ru.iFellow.Utility.EmuDriver;
 
 public abstract class BasePage {
     protected final AndroidDriver driver;
+    protected final By searchInput = By.xpath("//*[@resource-id='org.wikipedia:id/search_src_text']");
+    protected final By searchQuery = By.xpath("//*[@resource-id='org.wikipedia:id/search_container']");
 
     public BasePage() {
         this.driver = EmuDriver.getDriver();
--- src/main/java/ru/iFellow/Pages/MainPage.java
+++ src/main/java/ru/iFellow/Pages/MainPage.java
@@ -1,10 +1,6 @@
 package ru.iFellow.Pages;
 
-import org.openqa.selenium.By;
-
 public class MainPage extends BasePage {
-    protected final By searchInput = By.xpath("//*[@resource-id='org.wikipedia:id/search_src_text']");
-    private final By searchQuery = By.xpath("//*[@resource-id='org.wikipedia:id/search_container']");
 
     public void searchFor(String searchText) {
         driver.findElement(searchQuery).click();
